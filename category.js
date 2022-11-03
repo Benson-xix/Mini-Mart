2ba3ca931a3bce5aca6d6e359ecaad70ede94fb8
uzoefuna benson dominic <uzoefunam.benson@gmail.com>
2022-11-03 11:53:57 +0100

    Add files via upload

---
+++ category.js
@@ -0,0 +1,11 @@
+export const BASE_URL = "https://mini-mart-api.herokuapp.com";
+
+export const getAllCategories = async () => {
+  try {
+    const request = await fetch(`${BASE_URL}/api/category`);
+    const response = await request.json();
+    return response;
+  } catch (e) {
+    return { error: e.message };
+  }
+};
